9667070dd5697cec6e7ff49119b5e3689cb8dbc7
MargaritaVin <ritasergienko@rambler.ru>
2022-10-13 21:59:16 +0300

    Changes in Images-List

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -3,20 +3,20 @@ import { useState, useEffect } from 'react';
 const ImagesList = () => {
     const [images, setImages] = useState([]);
     const [isLoading, setLoading] = useState(true);
-    const [activePage, setActivePage] = useState(1)
+    const [page, setPage] = useState(1)
 
     useEffect (() => {
         setLoading(true);
-        fetch(`https://picsum.photos/v2/list?page=${activePage}&limit=10`)
+        fetch(`https://picsum.photos/v2/list?page=${page}&limit=10`)
         .then((response) => response.json())
         .then((data) => {
             setImages((prevState) => [...prevState, ...data]);
             setLoading(false);
         });
-    }, [activePage]);
+    }, [page]);
 
     const handleShowImages = () => {
-        setActivePage(activePage + 1)
+        setPage(page + 1)
     };
 
     if(isLoading) {
@@ -28,7 +28,7 @@ const ImagesList = () => {
         <h1 className='heading'>Image Gallery</h1>
         <ul className='list-img'>
               {images.map(({id, download_url}) => (
-                 <li className='item-img' key={id}><img className='img' src={download_url} alt=""/></li>
+                 <li className='item-img' key={id}><img src={download_url} alt=""/></li>
              ))}
         </ul>
         <button className='btn' onClick={handleShowImages}>Show more</button>
